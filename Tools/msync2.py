f09176be14b12b348ec487fe56bbe0228f0616dd
Bengt Ove Sannes <dev@bovesan.com>
2016-12-30 09:19:34 +0100

    msync2.py: Snapshot

--- Tools/msync2.py
+++ Tools/msync2.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import threading
-import time
+import json
 import gobject
 import gtk
+import os
 import platform
 import subprocess
+import threading
+import time
 
 
 gobject.threads_init()
@@ -19,7 +21,7 @@ class MyThread(threading.Thread):
         window = self.window
         screen = self.window.get_screen()
         window.set_title("Mistika remote sync")
-        window.set_size_request(screen.get_height()-200, screen.get_height()-200)
+        window.set_size_request(screen.get_width()-200, screen.get_height()-200)
         window.set_border_width(20)
         window.set_position(gtk.WIN_POS_CENTER)
         if 'darwin' in platform.system().lower():
@@ -32,20 +34,225 @@ class MyThread(threading.Thread):
         self.status_bar.show()
         self.context_id = self.status_bar.get_context_id("Statusbar example")
 
-        self.label1 = gtk.Label()
-        vbox.pack_start(self.label1, False, False, 10)
+        hbox = gtk.HBox(False, 10)
+        hbox.pack_start(gtk.Label('Remote host:'), False, False, 0)
+        vbox.pack_start(hbox, False, False, 0)
 
-        self.label2 = gtk.Label("Label 2")
-        vbox.pack_start(self.label2, False, False, 10)
+        self.hostsTreeStore = gtk.TreeStore(str, str, str, int, str) # Name, url, color, underline
+        self.hostsTree = gtk.TreeView()
+        cell = gtk.CellRendererText()
+        cell.set_property("editable", True)
+        cell.connect('edited', self.on_host_edit, (self.hostsTreeStore, 0))
+        column = gtk.TreeViewColumn('', cell, text=0)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.hostsTree.append_column(column)
+        cell = gtk.CellRendererText()
+        cell.set_property("foreground", "gray")
+        cell.set_property("editable", True)
+        cell.connect('edited', self.on_host_edit, (self.hostsTreeStore, 1))
+        column = gtk.TreeViewColumn('Host', cell, text=1)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.hostsTree.append_column(column)
+        cell = gtk.CellRendererText()
+        cell.set_property("foreground", "gray")
+        cell.set_property("editable", True)
+        cell.connect('edited', self.on_host_edit, (self.hostsTreeStore, 2))
+        column = gtk.TreeViewColumn('Username', cell, text=2)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.hostsTree.append_column(column)
+        cell = gtk.CellRendererText()
+        cell.set_property("foreground", "gray")
+        cell.set_property("editable", True)
+        cell.connect('edited', self.on_host_edit, (self.hostsTreeStore, 3))
+        column = gtk.TreeViewColumn('Port', cell, text=3)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.hostsTree.append_column(column)
+        cell = gtk.CellRendererText()
+        cell.set_property("foreground", "gray")
+        cell.set_property("editable", True)
+        cell.connect('edited', self.on_host_edit, (self.hostsTreeStore, 4))
+        column = gtk.TreeViewColumn('Projects path', cell, text=4)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.hostsTree.append_column(column)
+        hostsTreeStore = self.hostsTreeStore
+        #hostsTreeStore.append(None, ["Horten", 'horten.hocusfocus.no', 'mistika', 22, '/Volumes/SLOW_HF/PROJECTS/'])
+        #hostsTreeStore.append(None, ["Oslo", 's.hocusfocus.no', 'mistika', 22, '/Volumes/SLOW_HF/PROJECTS/'])
+        self.hosts_populate(hostsTreeStore)
+        linksFilter = hostsTreeStore.filter_new();
+        self.hostsTree.set_model(hostsTreeStore)
+        self.hostsTree.expand_all()
 
-        button = gtk.Button("Click me")
-        button.connect("clicked", self.button_click)
-        vbox.pack_start(button, False, False, 10)
+
+        scrolled_window = gtk.ScrolledWindow()
+        scrolled_window.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        scrolled_window.add(self.hostsTree)
+        #vbox.pack_start(scrolled_window)
+        vbox.pack_start(scrolled_window)
+
+        hbox = gtk.HBox(False, 10)
+        button = gtk.Button('+')
+        button.set_size_request(30, 30)
+        button.connect("clicked", self.add_host)
+        hbox.pack_end(button, False, False, 0)
+        button = gtk.Button('-')
+        button.set_size_request(30, 30)
+        button.connect("clicked", self.remove_host)
+        hbox.pack_end(button, False, False, 0)
+
+        button = gtk.Button('List projects')
+        button.connect("clicked", self.on_host_select)
+        hbox.pack_start(button, False, False, 0)
+
+
+        vbox.pack_start(hbox, False, False, 0)
+        self.projectsTreeStore = gtk.TreeStore(str) # Path
+        self.projectsTree = gtk.TreeView()
+        self.project_cell = gtk.CellRendererText()
+        project_cell = self.project_cell
+        project_cell.set_property('foreground', '#cccccc')
+        project_cell.set_property('style', 'italic')
+        #cell.connect('edited', self.on_host_edit, (self.projectsTreeStore, 0))
+        column = gtk.TreeViewColumn('', project_cell, text=0)
+        column.set_resizable(True)
+        column.set_expand(True)
+        self.projectsTree.append_column(column)
+        projectsTreeStore = self.projectsTreeStore
+        #hostsTreeStore.append(None, ["Horten", 'horten.hocusfocus.no', 'mistika', 22, '/Volumes/SLOW_HF/PROJECTS/'])
+        #hostsTreeStore.append(None, ["Oslo", 's.hocusfocus.no', 'mistika', 22, '/Volumes/SLOW_HF/PROJECTS/'])
+        #self.hosts_populate(projectsTreeStore)
+        projectsTreeStore.append(None, ['Loading projects ...'])
+        self.projectsTree.set_model(projectsTreeStore)
+        self.projectsTree.expand_all()
+
+        scrolled_window = gtk.ScrolledWindow()
+        scrolled_window.set_policy(gtk.POLICY_NEVER, gtk.POLICY_AUTOMATIC)
+        scrolled_window.add(self.projectsTree)
+        #vbox.pack_start(scrolled_window)
+        vbox.pack_start(scrolled_window)
+
+
+
+        #menu = ['Sync project', 'Sync media']
+        footer = gtk.HBox(False, 10)
+        quitButton = gtk.Button('Quit')
+        quitButton.set_size_request(70, 30)
+        quitButton.connect("clicked", self.on_quit)
+        footer.pack_end(quitButton, False, False)
+
+        vbox.pack_end(footer, False, False, 10)
 
         window.add(vbox)
         window.show_all()
         window.connect("destroy", self.on_quit)
         self.quit = False
+
+    def on_host_select(self, widget):
+        selection = self.hostsTree.get_selection()
+        (model, iter) = selection.get_selected()
+        print model[iter][0]
+        t = threading.Thread(target=self.list_projects, args=[model[iter][1], model[iter][2], model[iter][3], model[iter][4]])
+        self.threads.append(t)
+        t.setDaemon(True)
+        t.start()
+
+    def list_projects(self, address, user, port, projects_path):
+        cmd = ['ssh', '-oBatchMode=yes', '-p', str(port), '%s@%s' % (user, address), 'ls -xd %s/*/' % projects_path]
+        try:
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            projects = output.splitlines()
+        except:
+            raise
+            return
+        self.projectsTreeStore.clear()
+        self.project_cell.set_property('foreground', '#000000')
+        self.project_cell.set_property('style', 'normal')
+        for project_path in projects:
+            project_name = project_path.strip('/').split('/')[-1]
+            self.projectsTreeStore.append(None, [project_name])
+        # cmd = ['ssh', '-p', str(port), '%s@%s' % (user, address), 'grep MISTIKA_WORK MISTIKA-ENV/MISTIKA_WORK']
+        # output = subprocess.check_output(cmd)
+        # projects_path = output.splitlines()[0].split()[1]
+        #print projects_path
+
+    def add_host(self, widget):
+        self.hostsTreeStore.append(None, ['New host', '', 'mistika', 22, ''])
+
+    def remove_host(self, widget):
+        selection = self.hostsTree.get_selection()
+        (model, iter) = selection.get_selected()
+        try:
+            model.remove(iter)
+            self.hosts_store()
+        except:
+            raise
+
+        #self.hostsTreeStore.append(None, ['New host', '', 'mistika', 22, ''])
+
+    #def on_host_edit(self, cellrenderertoggle, path, *ignore):
+    def on_host_edit(self, cell, path, new_text, user_data):
+        tree, column = user_data
+        print tree[path][column],
+        tree[path][column] = new_text
+        print '-> ' + tree[path][column]
+        self.hosts_store()
+
+    def error(self, message):
+        dialog = gtk.MessageDialog(parent=self, 
+                            flags=gtk.DIALOG_MODAL, 
+                            type=gtk.MESSAGE_ERROR, 
+                            buttons=gtk.BUTTONS_NONE, 
+                            message_format=None)
+        dialog.set_markup(message)
+        dialog.run()
+
+    def hosts_populate(self, tree):
+        cfg_path = os.path.expanduser('~/.mistika-hyperspeed/sync/hosts.json')
+        try:
+            hosts = json.loads(open(cfg_path).read())
+        except IOError as e:
+            return
+        #print repr(hosts)
+        for host in hosts:
+            tree.append(None, [host, hosts[host]['address'], hosts[host]['user'], hosts[host]['port'], hosts[host]['path']])
+        status = 'Loaded hosts.'
+        self.status_bar.push(self.context_id, status)
+
+
+    def hosts_store(self):
+        tree = self.hostsTreeStore
+        cfg_path = os.path.expanduser('~/.mistika-hyperspeed/sync/hosts.json')
+        hosts = {}
+        for row in tree:
+            alias = row[0]
+            for value in row:
+                print value,
+            print ''
+            hosts[alias] = {}
+            hosts[alias]['address'] = row[1]
+            hosts[alias]['user'] = row[2]
+            hosts[alias]['port'] = row[3]
+            hosts[alias]['path'] = row[4]
+        cfg_path_parent = os.path.dirname(cfg_path)
+        if not os.path.isdir(cfg_path_parent):
+            try:
+                os.makedirs(cfg_path_parent)
+            except IOError as e:
+                self.error('Could not create config folder:\n'+cfg_path_parent)
+                return
+        try:
+            open(cfg_path, 'w').write(json.dumps(hosts))
+            status = 'Wrote to %s' % cfg_path
+            print status
+            self.status_bar.push(self.context_id, status)
+        except IOError as e:
+            self.error('Could not write to file:\n'+cfg_path)
+        except:
+            raise
 
     def on_quit(self, widget):
         print 'Closed by: ' + repr(widget)
@@ -58,12 +265,8 @@ class MyThread(threading.Thread):
         return False
 
     def run(self):
-        counter = 0
-        while not self.quit:
-            counter += 1
-            counter_str = "Counter: %i" % counter
-            gobject.idle_add(self.update_label, counter_str, self.label1)
-            time.sleep(0.1)
+        selection = self.hostsTree.get_selection()
+        selection.unselect_all()
 
     def worker(self, *task):
         task[0](task[1:])
