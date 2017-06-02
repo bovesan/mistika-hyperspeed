b2f34903de108a718fc2602d221c59a6fcbe7092
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-06-02 13:48:48 +0200

    Tests: Created dashboard launch test

--- hyperspeed/tests.py
+++ hyperspeed/tests.py
@@ -3,11 +3,26 @@
 import unittest
 import sys
 import os
+import subprocess
+import time
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+root_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root_folder)
 import hyperspeed
+os.chdir(root_folder)
 
-class StackTest(unittest.TestCase):
+class DashboardTests(unittest.TestCase):
+    def test_launch(self):
+    	program_path = './hyperspeed-dashboard.py'
+    	p = subprocess.Popen(['./hyperspeed-dashboard.py'])
+    	time.sleep(1)
+    	p.poll()
+    	if p.returncode > 0:
+    		self.fail('Crashed: %s' % program_path)
+    	else:
+    		p.kill()
+
+class StackTests(unittest.TestCase):
     def test_dependencies_attribute(self):
     	stack = hyperspeed.Stack('Samples/hill14.env')
     	self.maxDiff = None
