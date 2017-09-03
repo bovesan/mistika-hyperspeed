2f073c5c076827ba97047bf24353e30860624794
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-09-03 18:22:21 +0200

    Bugfix: utils.reveals_file() would not work if launched from Mistika session

--- hyperspeed/utils.py
+++ hyperspeed/utils.py
@@ -5,15 +5,29 @@ import subprocess
 import os
 
 def reveal_file(path):
-    folder = os.path.dirname(path)
-    print 'Reveal: ', folder
-    if platform.system() == "Windows":
-        subprocess.Popen(["explorer", "/select,", path])
-    elif platform.system() == "Darwin":
-        subprocess.Popen(["open", '-R', path])
-    else:
-        os.setsid()
-        try:
-            subprocess.Popen(["dolphin", '--select', path])
-        except OSError:
-            subprocess.Popen(["xdg-open", folder])
+    if isinstance(path, basestring): # Single path
+        paths = [path]
+    else: # Multiple paths
+        paths = path
+    folders = {}
+    for path in paths:
+        folder = os.path.dirname(path)
+        folders[folder] = path
+    for folder, path in folders.iteritems():
+        print 'Reveal: ', folder
+        if platform.system() == "Windows":
+            subprocess.Popen(["explorer", "/select,", path])
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", '-R', path])
+        else:
+            try:
+                os.setsid()
+            except OSError:
+                pass
+            dolphinEnv = os.environ.copy()
+            del dolphinEnv["LD_LIBRARY_PATH"]
+            try:
+                subprocess.Popen(["dolphin", '--select', path], env=dolphinEnv)
+            except OSError:
+                subprocess.Popen(["xdg-open", folder])
+                
