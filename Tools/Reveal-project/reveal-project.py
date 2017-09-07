d9ea41ac0518e0cfe7961405e4c202148a458bf8
Bengt Ove Sannes <dev@bovesan.com>
2017-09-07 13:49:21 +0200

    Bugfix: Failed if run from a shortcut

--- Tools/Reveal-project/reveal-project.py
+++ Tools/Reveal-project/reveal-project.py
@@ -6,10 +6,11 @@ import sys
 
 try:
     cwd = os.getcwd()
-    os.chdir(os.path.dirname(sys.argv[0]))
+    os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
     sys.path.append("../..")
     from hyperspeed import mistika
     import hyperspeed.utils
+    print 'Revealing project:', mistika.project
     hyperspeed.utils.reveal_file(os.path.join(mistika.projects_folder, mistika.project))
 except ImportError:
-    "Could not load Hyperspeed modules"
+    print "Could not load Hyperspeed modules"
