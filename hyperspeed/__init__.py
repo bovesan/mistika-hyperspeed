bb151a1d92e3972298eb10456ff2e7cac290df34
Bengt Ove Sannes <dev@bovesan.com>
2017-09-24 22:05:21 +0200

    Added folder attribute to hyperspeed module

--- hyperspeed/__init__.py
+++ hyperspeed/__init__.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+import os
 import mistika
 import manage
 from stack import Stack
 
+
+folder = os.getcwd()
+while not 'hyperspeed-dashboard.py' in os.listdir(folder) and not folder == '/':
+    folder = os.path.dirname(folder)
