c5c068f96a8e290d4f07afd353f8259608b66ca9
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-10-04 22:16:45 +0200

    Afterscript tidywork. Testet OK on CentOS

--- Afterscripts/Rewrap-to-mov/rewrap-to-mov.py
+++ Afterscripts/Rewrap-to-mov/rewrap-to-mov.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
-# try:
-#     os.setsid()
-# except OSError as e:
-#     print e
 
-import hyperspeed
 import hyperspeed.afterscript
 
 title          = 'Rewrap to mov'
@@ -18,23 +12,3 @@ output_pattern = '/Volumes/SAN3/Masters/<project>/<project>_<render_name>/<proje
 
 hyperspeed.afterscript.AfterscriptFfmpeg(__file__, cmd, output_pattern, title)
 hyperspeed.afterscript.gtk.main()
-
-# env = os.environ.copy()
-# env.clear()
-# env = {
-# 	'LESS': '-M -I',
-#     'CPU': 'x86_64',
-#     'KDE_FULL_SESSION': 'true',
-#     'INFOPATH': '/usr/local/info:/usr/share/info:/usr/info',
-#     'SHELL': '/bin/tcsh',
-
-# }
-# cmd = [
-# 	os.path.join(hyperspeed.folder, 'hyperspeed/afterscript.py'),
-# 	'AfterscriptFfmpeg',
-# 	__file__,
-# 	cmd,
-# 	output_pattern,
-# 	title
-# ]
-# subprocess.call(cmd, env=env)
