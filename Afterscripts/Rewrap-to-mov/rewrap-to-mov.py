2beca725ace931f6c155441e5542f3ff03c25430
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-10-04 22:48:32 +0200

    Settings work
    Pattern is currently not used correctly.\nSettings are not stored.

--- Afterscripts/Rewrap-to-mov/rewrap-to-mov.py
+++ Afterscripts/Rewrap-to-mov/rewrap-to-mov.py
@@ -6,9 +6,9 @@ import hyperspeed.afterscript
 title          = 'Rewrap to mov'
 cmd            = '-vcodec copy -acodec copy'
 # Path relative to primary output folder of render:
-# output_pattern = '<project>_<render_name>.<codec>.mov'
+# default_output = '<project>_<render_name>.<codec>.mov'
 # Absolute path:
-output_pattern = '/Volumes/SAN3/Masters/<project>/<project>_<render_name>/<project>_<render_name>.<codec>.mov'
+default_output = '/Volumes/SAN3/Masters/<project>/<project>_<render_name>/<project>_<render_name>.<codec>.mov'
 
-hyperspeed.afterscript.AfterscriptFfmpeg(__file__, cmd, output_pattern, title)
+hyperspeed.afterscript.AfterscriptFfmpeg(__file__, cmd, default_output, title)
 hyperspeed.afterscript.gtk.main()
