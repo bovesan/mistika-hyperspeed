a834dd1e65ddc28e2e1c1d93444ea64edabee0f9
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-10-07 22:19:36 +0200

    Reordering functionality

--- Tools/Render-manager/render-manager.py
+++ Tools/Render-manager/render-manager.py
@@ -31,39 +31,53 @@ THIS_HOST_ALIAS = 'This machine'
 OTHER_HOSTS_ALIAS = 'Others'
 THREAD_LIMIT = multiprocessing.cpu_count()
 HOSTNAME = socket.gethostname()
+UPDATE_RATE = 1.0
 
 class RenderItem(hyperspeed.stack.Render):
-    settings = {}
     treeview = None
     treestore = None
     row_reference = None
     afterscript_progress = 0.0
     owner = 'Unknown'
-    settings = {
-        'stage': 'render',
-        'afterscript' : None,
-        'status' : 'Not started',
-        'frames_rendered' : 0.0,
-        'afterscript_progress' : 0.0,
-    }
     description = None
     def __init__(self, path, uid):
         super(RenderItem, self).__init__(path)
+        self.settings = {
+            'stage': 'render',
+            'afterscript' : None,
+            'status' : 'Not started',
+            'frames_rendered' : 0,
+            'afterscript_progress' : 0.0,
+        }
         self.uid = uid
+        self.settings['priority'] = self.ctime
         self.duration = hyperspeed.video.frames2tc(self.frames, self.fps)
     def do_render(self):
         cmd = ['mistika', '-r', self.path]
-        # self.logfile_path = self.path + '.log'
-        # logfile_h = open(self.logfile_path, 'w')
+        log_path = self.path + '.log'
+        logfile_h = open(log_path, 'w')
         # self.process = subprocess.Popen(cmd, stdout=logfile_h, stderr=subprocess.STDOUT)
+        # total time: 4.298 sec, 0.029 sec per frame, 34.665 frames per sec
+        self.set_settings({
+            'rendered_start_time' : time.time()
+        })
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        while proc.returncode == None:
-            line = proc.stdout.readline()
-            if line.startswith('Done:'):
-                self.set_settings({
-                    'frames_rendered' : line.split(' ')[1].strip(',')
-                })
-            proc.poll()
+        with open(log_path, 'w') as log:
+            while proc.returncode == None:
+                line = proc.stdout.readline()
+                log.write(line)
+                if line.startswith('Done:'):
+                    self.set_settings({
+                        'frames_rendered' : int(line.split(' ')[1].strip(','))
+                    })
+                elif line.startswith('total time:'):
+                    self.set_settings({
+                        'frames_rendered' : int(self.frames),
+                        'render_end_time' : time.time(),
+                        'render_elapsed_time' : float(line.split()[2]),
+                        'status' : 'Render complete',
+                    })
+                proc.poll()
         # logfile_h.flush()
     def gui_remove(self, item):
         row_path = self.row_reference.get_path()
@@ -71,7 +85,11 @@ class RenderItem(hyperspeed.stack.Render):
         treestore.remove(iter)
     def set_settings(self, settings):
         self.settings.update(settings)
-        open(self.settings_path, 'w').write(json.dumps(self.settings, indent=4))
+        open(self.settings_path, 'w').write(json.dumps(self.settings, indent=4, sort_keys=True))
+        gobject.idle_add(self.gui_update)
+    def gui_update(self):
+        row_path = self.row_reference.get_path()
+        self.treestore[row_path] = self.treestore_values
     @property
     def render_progress(self):
         try:
@@ -85,28 +103,30 @@ class RenderItem(hyperspeed.stack.Render):
                 self.uid, # Id
                 self.project, # Project
                 self.name, # Name
-                self.render_progress, # Progress
+                self.render_progress * 100.0, # Progress
                 '%5.2f%%' % (self.render_progress * 100.0), # Progress str
                 self.settings['status'], # Status
                 self.settings['afterscript'], # Afterscript
                 self.ctime, # Added time
                 self.description, # Description
                 hyperspeed.human.time(self.ctime), # Human time
-                self.render_progress > 0 # Progress visible
+                self.render_progress > 0, # Progress visible
+                self.settings['priority'],
             ]
         else:
             return [
                 self.uid, # Id
                 self.project, # Project
                 self.name, # Name
-                self.afterscript_progress, # Progress
+                self.afterscript_progress * 100.0, # Progress
                 '%5.2f%%' % (self.afterscript_progress * 100.0), # Progress str
                 self.status, # Status
                 self.settings['afterscript'], # Afterscript
                 self.ctime, # Added time
                 self.description, # Description
                 hyperspeed.human.time(self.ctime), # Human time
-                self.afterscript_progress > 0 # Progress visible
+                self.afterscript_progress > 0, # Progress visible
+                self.settings['priority'],
             ]
 
 
@@ -143,6 +163,7 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         for afterscript in hyperspeed.afterscript.list():
             self.afterscripts_model.append([afterscript])
 
+        vbox.pack_start(self.settings_panel(), False, False, 10)
         vbox.pack_start(self.init_render_queue_window())
         # vbox.pack_start(self.init_afterscript_queue_window())
 
@@ -160,6 +181,45 @@ class RenderManagerWindow(hyperspeed.ui.Window):
 
         self.comboEditable = None
         gobject.idle_add(self.bring_to_front)
+        gobject.timeout_add(1000, self.gui_periodical_updates)
+    def settings_panel(self):
+        expander = gtk.Expander('Settings')
+        vbox = gtk.VBox()
+        hbox = gtk.HBox()
+        label =  gtk.Label('Shared queue:')
+        hbox.pack_start(label, False, False, 5)
+        entry = self.shared_queue_entry = gtk.Entry()
+        # entry.set_text(self.cmd_string)
+        hbox.pack_start(entry)
+        button = self.shared_queue_pick_button = gtk.Button('...')
+        button.connect("clicked", self.on_folder_pick, entry, 'Select shared queue folder')
+        hbox.pack_start(button, False, False, 5)
+        vbox.pack_start(hbox, False, False, 5)
+        vbox.pack_start(gtk.HSeparator(), False, False, 10)
+        expander.add(vbox)
+        return expander
+    def gui_periodical_updates(self):
+        self.launch_thread(self.io_populate_render_queue)
+        return True # Must return true to keep repeating
+    def on_folder_pick(self, widget, entry, title):
+        folder = entry.get_text()
+        if not os.path.isdir(folder):
+            folder = os.path.join(mistika.projects_folder)
+        dialog = gtk.FileChooserDialog(
+            title=title,
+            parent=None,
+            action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK),
+            backend=None
+        )
+        dialog.add_shortcut_folder(folder)
+        dialog.set_current_folder(folder)
+        response = dialog.run()
+        if response == gtk.RESPONSE_OK:
+            entry.set_text(dialog.get_filename())
+        elif response == gtk.RESPONSE_CANCEL:
+            print 'Closed, no files selected'
+        dialog.destroy()
     def bring_to_front(self):
         self.present()
     def config_rw(self, write=False):
@@ -167,25 +227,28 @@ class RenderManagerWindow(hyperspeed.ui.Window):
     def init_render_queue_window(self):
         self.render_queue = {}
         row_references = self.row_references_render = {}
-        tree           = self.render_treeview  = gtk.TreeView()
+        treeview       = self.render_treeview  = gtk.TreeView()
         treestore      = self.render_treestore = gtk.TreeStore(
-            str, # Id
-            str, # Project
-            str, # Name
-            int, # Progress
-            str, # Progress str
-            str, # Status
-            str, # Afterscript
-            str, # Added time
-            str, # Description
-            str, # Human time
-            bool # Progress visible
+            str,  # 00 Id
+            str,  # 01 Project
+            str,  # 02 Name
+            int,  # 03 Progress
+            str,  # 04 Progress str
+            str,  # 05 Status
+            str,  # 06 Afterscript
+            float,# 07 Added time
+            str,  # 08 Description
+            str,  # 09 Human time
+            bool, # 10 Progress visible
+            float,# 11 Priority
         )
+        treestore.set_sort_column_id(11, gtk.SORT_ASCENDING)
         tree_filter    = self.render_queue_filter    = treestore.filter_new();
         # tree.connect('button-press-event' , self.button_press_event)
         for queue_name in [THIS_HOST_ALIAS, OTHER_HOSTS_ALIAS]:
-            row_iter = treestore.append(None, [queue_name, queue_name, '', 0, '', '', '', '', 'Render jobs submitted by %s' % queue_name.lower(), '', False])
+            row_iter = treestore.append(None, None)
             row_path = treestore.get_path(row_iter)
+            treestore[row_path][1] = queue_name
             row_references[queue_name] = gtk.TreeRowReference(treestore, row_path)
         vbox = gtk.VBox(False, 10)
         headerBox = gtk.HBox(False, 5)
@@ -211,21 +274,21 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         column = gtk.TreeViewColumn('Project', gtk.CellRendererText(), text=1)
         column.set_resizable(True)
         column.set_expand(False)
-        tree.append_column(column)
+        treeview.append_column(column)
         column = gtk.TreeViewColumn('Name', gtk.CellRendererText(), text=2)
         column.set_resizable(True)
         column.set_expand(False)
-        tree.append_column(column)
+        treeview.append_column(column)
         cell = gtk.CellRendererProgress()
         column = gtk.TreeViewColumn('Progress', cell, value=3, text=4)
         column.add_attribute(cell, 'visible', 10)
         column.set_resizable(True)
         column.set_expand(False)
-        tree.append_column(column)
+        treeview.append_column(column)
         column = gtk.TreeViewColumn('Status', gtk.CellRendererText(), text=5)
         column.set_resizable(True)
         column.set_expand(True)
-        tree.append_column(column)
+        treeview.append_column(column)
 
         cell = gtk.CellRendererCombo()
         cell.set_property("editable", True)
@@ -238,13 +301,13 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         column = gtk.TreeViewColumn("Afterscript", cell, text=6)
         column.set_resizable(True)
         column.set_expand(False)
-        tree.append_column(column)
+        treeview.append_column(column)
         column = gtk.TreeViewColumn('Added time', gtk.CellRendererText(), text=9)
         column.set_resizable(True)
         column.set_expand(False)
-        tree.append_column(column)
-        tree.set_tooltip_column(8)
-        tree.set_rules_hint(True)
+        treeview.append_column(column)
+        treeview.set_tooltip_column(8)
+        treeview.set_rules_hint(True)
         # it = queueTreestore.append(None, ["Private (6)", '', '', '', '', 0, ''])
         # queueTreestore.append(it, ["RnD", 'test_0001', 'Rendering on gaia', 'gaia', '08:27', 20, '20%'])
         # queueTreestore.append(it, ["RnD", 'test_0001', 'Queued', 'gaia', '08:27', 0, ''])
@@ -255,25 +318,27 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         # it = queueTreestore.append(None, ["Public (2)", '', '', '', '', 0, ''])
         # queueTreestore.append(it, ["Mastering", 'film01', 'Queued', 'apollo2', '08:27', 0, ''])
         # queueTreestore.append(it, ["Mastering", 'film02', 'Queued', 'apollo2', '08:27', 0, ''])
-        tree.set_model(treestore)
-        tree.expand_all()
+        treeview.set_model(treestore)
+        treeview.expand_all()
         scrolled_window = gtk.ScrolledWindow()
         scrolled_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-        scrolled_window.add(tree)
+        scrolled_window.add(treeview)
         afterscriptsBox.pack_start(scrolled_window)
-        afterscriptsButtons = gtk.VBox(False, 3)
-        afterscriptsButtons.set_size_request(30,80)
+        vbox_move = gtk.VBox(False, 3)
+        vbox_move.set_size_request(30,80)
         gtk.stock_add([(gtk.STOCK_GO_UP, "", 0, 0, "")])
-        upButton = gtk.Button(stock=gtk.STOCK_GO_UP)
-        afterscriptsButtons.pack_start(upButton)
+        button = gtk.Button(stock=gtk.STOCK_GO_UP)
+        vbox_move.pack_start(button)
+        button.connect('clicked', self.on_move, -1, treeview)
         gtk.stock_add([(gtk.STOCK_GO_DOWN, "", 0, 0, "")])
-        downButton = gtk.Button(stock=gtk.STOCK_GO_DOWN)
-        afterscriptsButtons.pack_start(downButton)
-        afterscriptsBox.pack_start(afterscriptsButtons, False, False)
+        button = gtk.Button(stock=gtk.STOCK_GO_DOWN)
+        button.connect('clicked', self.on_move, +1, treeview)
+        vbox_move.pack_start(button)
+        afterscriptsBox.pack_start(vbox_move, False, False)
 
         menu = self.popup_menu = gtk.Menu()
-        newi = gtk.ImageMenuItem(gtk.STOCK_DELETE)
-        newi.connect("activate", self.on_render_delete)
+        newi = gtk.ImageMenuItem(gtk.STOCK_INFO)
+        newi.connect("activate", self.on_render_info)
         newi.show()
         menu.append(newi)
         newi = gtk.ImageMenuItem(gtk.STOCK_MEDIA_PLAY)
@@ -281,12 +346,51 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         newi.connect("activate", self.on_render_start)
         newi.show()
         menu.append(newi)
+        newi = gtk.ImageMenuItem(gtk.STOCK_DELETE)
+        newi.connect("activate", self.on_render_delete)
+        newi.show()
+        menu.append(newi)
         menu.set_title('Popup')
-        tree.connect('button_release_event', self.on_render_button_press_event, tree)
+        treeview.connect('button_release_event', self.on_render_button_press_event, treeview)
 
         self.launch_thread(self.io_populate_render_queue)
         vbox.pack_start(afterscriptsBox, True, True, 5)
         return vbox
+    def on_move(self, widget, direction, treeview):
+        selection = treeview.get_selection()
+        (treestore, row_paths) = selection.get_selected_rows()
+        for row_path in sorted(row_paths, reverse=True):
+            file_id = treestore[row_path][0]
+            render = self.renders[file_id]
+            row_path_parent = row_path[:-1]
+            row_path_lastbit = row_path[1]
+            if direction < 0 and row_path_lastbit == 0:
+                return
+            path_one_away = row_path_parent+(row_path_lastbit+direction,)
+            path_two_away = row_path_parent+(row_path_lastbit+(direction*2),)
+            try:
+                priority_one_away = treestore[path_one_away][11]
+            except IndexError:
+                return
+            try:
+                priority_two_away = treestore[path_two_away][11]
+            except IndexError:
+                priority_two_away = priority_one_away+(direction*0.01)
+            priority_between = priority_one_away - ((priority_one_away - priority_two_away) * 0.5)
+            render.set_settings({
+                'priority' :  priority_between
+            })
+    def gui_info_dialog(self, message):
+        dialog = gtk.MessageDialog(
+            parent = self,
+            flags=0,
+            type=gtk.MESSAGE_INFO,
+            buttons=gtk.BUTTONS_OK,
+            message_format=message
+        )
+        dialog.set_position(gtk.WIN_POS_CENTER)
+        response = dialog.run()
+        dialog.destroy()
     def init_afterscript_queue_window(self):
         self.render_queue = {}
         row_references = self.row_references_afterscript = {}
@@ -462,7 +566,6 @@ class RenderManagerWindow(hyperspeed.ui.Window):
             treestore = render.treeview.get_model()
             parent_row_path = parent_row_reference.get_path()
             parent_row_iter = treestore.get_iter(parent_row_path)
-            print repr(treestore), repr(render.treestore)
             if not render.treestore == treestore:
                 if render.treestore != None:
                     row_path = render.row_reference.get_path()
@@ -473,8 +576,7 @@ class RenderManagerWindow(hyperspeed.ui.Window):
                 render.row_reference = gtk.TreeRowReference(treestore, row_path)
                 render.treestore = treestore
             else:
-                row_path = render.row_reference.get_path()
-                render.treestore[row_path] = render.treestore_values
+                render.gui_update()
         self.render_treeview.expand_all()
         # self.afterscript_tree.expand_all()
     def launch_thread(self, target, name=False, args=[], kwargs={}):
@@ -509,6 +611,15 @@ class RenderManagerWindow(hyperspeed.ui.Window):
       e.send_event = True
       e.in_ = False
       self.comboEditable.emit('focus-out-event', e)
+    def on_render_info(self, widget, *ignore):
+        treestore = self.render_treestore
+        treepath = self.render_queue_selected_path
+        path = treestore[treepath][0]
+        render = self.renders[path]
+        message = render.uid
+        for k, v in render.settings.iteritems():
+            message += '\n%s: %s' % (k, v)
+        self.gui_info_dialog(message)
     def on_render_delete(self, widget, *ignore):
         treestore = self.render_treestore
         treepath = self.render_queue_selected_path
