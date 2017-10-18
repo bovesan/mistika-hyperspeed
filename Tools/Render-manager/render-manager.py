97a5b2de825d421ea2ff970a9cd5ae4af01ff2e7
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-10-18 10:28:41 +0200

    Fixed delayed deletions being queued multiple times until removed

--- Tools/Render-manager/render-manager.py
+++ Tools/Render-manager/render-manager.py
@@ -1164,10 +1164,18 @@ class RenderManagerWindow(hyperspeed.ui.Window):
         return vbox
     def try_rmdir(self, file_path):
         try:
+            self.files_to_be_deleted.remove(file_path)
+        except ValueError:
+            return
+        try:
             os.rmdir(file_path)
         except OSError:
             pass
     def try_remove(self, file_path):
+        try:
+            self.files_to_be_deleted.remove(file_path)
+        except ValueError:
+            return
         try:
             os.remove(file_path)
         except OSError:
@@ -1175,6 +1183,10 @@ class RenderManagerWindow(hyperspeed.ui.Window):
     def delete_orphaned(self, file_path):
         # Should not be done imidiately because others nodes might be operating.
         # gobject.timeout_add(5000, self.delete_orphaned, file_path)
+        try:
+            self.files_to_be_deleted.remove(file_path)
+        except ValueError:
+            return
         file_id, file_ext = os.path.splitext(file_path)
         rnd_path = file_id+'.rnd'
         if not os.path.exists(rnd_path):
@@ -1183,6 +1195,11 @@ class RenderManagerWindow(hyperspeed.ui.Window):
             except OSError:
                 pass
     def io_parse_queue_folder(self, queue_folder, local=False):
+        try:
+            self.files_to_be_deleted
+        except AttributeError:
+            self.files_to_be_deleted = []
+        files_to_be_deleted = self.files_to_be_deleted
         renders = self.renders
         hostname = socket.gethostname()
         for queue_name in os.listdir(queue_folder):
@@ -1196,23 +1213,26 @@ class RenderManagerWindow(hyperspeed.ui.Window):
             try:
                 for root, dir_names, file_names in os.walk(queue_path, topdown=True):
                     if file_names == dir_names == []:
-                        gobject.timeout_add(5000, self.try_rmdir, root)
-                    # for dir_name in dir_names:
-                    #     if dir_name.startswith('.'):
-                    #         dir_names.remove(dir_name)
+                        if not file_path in files_to_be_deleted:
+                            files_to_be_deleted.append(file_path)
+                            gobject.timeout_add(5000, self.try_rmdir, root)
                     for file_name in file_names:
                         file_path = os.path.join(root, file_name)
                         file_id, file_ext = os.path.splitext(file_path)
                         if '/.trash/' in file_path:
                             # Delete if still there in 60 seconds
-                            gobject.timeout_add(60*1000, self.try_remove, file_path)
+                            if not file_path in files_to_be_deleted:
+                                files_to_be_deleted.append(file_path)
+                                gobject.timeout_add(60*1000, self.try_remove, file_path)
                             continue
                         if file_name == 'priority.cfg':
                             continue
                         if file_ext != '.rnd':
                             rnd_path = file_id+'.rnd'
                             if not os.path.exists(rnd_path):
-                                gobject.timeout_add(5000, self.delete_orphaned, file_path)
+                                if not file_path in files_to_be_deleted:
+                                    files_to_be_deleted.append(file_path)
+                                    gobject.timeout_add(5000, self.delete_orphaned, file_path)
                             continue
                         render = RenderItem(file_path, self.settings, renders)
                         if not render.path.endswith(render.id+'.rnd'):
