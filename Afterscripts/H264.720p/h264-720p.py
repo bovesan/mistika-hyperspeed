f9393f8ae2a0552e2d23862d42ca3ddd2a6267fd
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-11-06 14:58:30 +0100

    Use 709 matrix when converting rgb to yuv

--- Afterscripts/H264.720p/h264-720p.py
+++ Afterscripts/H264.720p/h264-720p.py
@@ -4,7 +4,7 @@
 import hyperspeed.afterscript
 
 title          = 'H264.720p'
-cmd            = '-vsync 0 -movflags faststart -crf 20 -minrate 0 -maxrate 4M -bufsize 15M -filter_complex "scale=1280:720,setsar=1" -pix_fmt yuv420p -vcodec libx264  -c:a aac -b:a 160k -strict -2 -ac 2 -ar 44100 -acodec aac -af pan=stereo:c0=c0:c1=c1'
+cmd            = '-vsync 0 -movflags faststart -crf 20 -minrate 0 -maxrate 4M -bufsize 15M -filter_complex "scale=1280:720:out_color_matrix=bt709,setsar=1" -pix_fmt yuv420p -vcodec libx264 -c:a aac -b:a 160k -strict -2 -ac 2 -ar 44100 -acodec aac -af pan=stereo:c0=c0:c1=c1'
 # Path relative to primary output folder of render:P
 # default_output = '[project]_[render_name].[codec].mov'
 # Absolute path:
