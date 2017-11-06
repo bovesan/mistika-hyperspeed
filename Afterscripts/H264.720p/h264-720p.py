a89cba2d6afd2218d8ded277adadb678caa2f3d2
Bengt Ove Sannes <bengtovesannes@gmail.com>
2017-11-06 14:10:34 +0100

    Changed H264 afterscript to aac codec

--- Afterscripts/H264.720p/h264-720p.py
+++ Afterscripts/H264.720p/h264-720p.py
@@ -4,8 +4,8 @@
 import hyperspeed.afterscript
 
 title          = 'H264.720p'
-cmd            = '-vsync 0 -movflags faststart -crf 20 -minrate 0 -maxrate 4M -bufsize 15M -filter_complex "scale=1280:720,setsar=1" -pix_fmt yuv420p -vcodec libx264  -c:a aac -b:a 160k -strict -2 -ac 2 -ar 44100 -acodec pcm_s16le -af pan=stereo:c0=c0:c1=c1'
-# Path relative to primary output folder of render:
+cmd            = '-vsync 0 -movflags faststart -crf 20 -minrate 0 -maxrate 4M -bufsize 15M -filter_complex "scale=1280:720,setsar=1" -pix_fmt yuv420p -vcodec libx264  -c:a aac -b:a 160k -strict -2 -ac 2 -ar 44100 -acodec aac -af pan=stereo:c0=c0:c1=c1'
+# Path relative to primary output folder of render:P
 # default_output = '[project]_[render_name].[codec].mov'
 # Absolute path:
 default_output = '/Volumes/SAN3/Masters/[project]/[project]_[rendername]/[project]_[rendername].h264.720p.mov'
