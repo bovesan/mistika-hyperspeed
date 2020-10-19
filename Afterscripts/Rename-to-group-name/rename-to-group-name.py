42ae137e7a684ba7492ec7027a42d3ade7873f99
Hocus Focus <dev@hocusfocus.no>
2020-10-19 15:38:32 +0200

    Renamescript WiP

---
+++ Afterscripts/Rename-to-group-name/rename-to-group-name.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os, time, subprocess
+
+try:
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
+    sys.path.append("../..")
+    import hyperspeed.human
+    import hyperspeed.mistika
+    import hyperspeed.stack
+except ImportError:
+    sys.exit()
+
+rnd_path = hyperspeed.mistika.get_rnd_path(sys.argv[2])
+render = hyperspeed.stack.Render(rnd_path)
+
+print 'Not yet implemented'
+sys.exit() 
+
+if '%' in render.output_video.path:
+	print 'Sequence support not implemented'
+	sys.exit() 
+else:
+	video_file_path = render.output_video.path
+videoFile_mtime = os.stat(video_file_path).st_mtime
+rndFile_mtime = os.stat(render.path).st_mtime
+diff = videoFile_mtime - rndFile_mtime
+
+message = '%s %s\n%s %s\n%s difference' % ( hyperspeed.human.time_of_day(rndFile_mtime), render.path, hyperspeed.human.time_of_day(videoFile_mtime), video_file_path, hyperspeed.human.duration(diff))
+
+subprocess.call(['xmessage', message])
