29a41aa7512a36d5affb720a1d5d05b2721cfc48
Mistika 2 <dev@hocusfocus.no>
2022-12-09 15:21:50 +0100

    New tool: Reveal central

---
+++ Tools/Reveal-central/reveal-central.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    cwd = os.getcwd()
+    os.chdir(os.path.dirname(os.path.realpath(sys.argv[0])))
+    sys.path.append("../..")
+    from hyperspeed import mistika
+    import hyperspeed.utils
+    print 'Revealing project:', mistika.project
+    hyperspeed.utils.reveal_file(os.path.join('/Volumes/Central/CentralProjects/', mistika.project))
+except ImportError:
+    print "Could not load Hyperspeed modules"
